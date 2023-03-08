dd11398d21903816f06581e4f55500b72479752a
skmhsp1 <hsp880514@gmail.com>
2023-03-08 17:30:25 +0900

    로그인 관련 user 관련 vo,dao, service, mybatis, config, login 수정, 추가

--- project/src/main/java/com/itkey/service/userService.java
+++ project/src/main/java/com/itkey/service/userService.java
@@ -1,5 +1,15 @@
 package com.itkey.service;
 
+import com.itkey.vo.userVO;
+
 public interface userService {
+
+	//phone check
+	userVO loginPhCheck(String loginPH1);
+	//pw check
+	userVO loginPwCheck2(userVO loginPW);
+	
+	
+	
 
 }
